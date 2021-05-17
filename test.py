cdc9f0a32c5e8ae54bf9dbe551485ff5c7903e60
bbslipers <slipers@mail.ru>
2021-05-17 12:16:10 +0300

    Updated from Brython Server: 17.05.2021 12:16:10

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("test")
+from ggame import App, ImageAsset, Sprite
+
+# Create a displayed object at 100,100 using an image asset
+Sprite(ImageAsset("bunny.png"), (100, 100))
+# Create the app, with a default stage
+APP = App()
+# Run the app
+APP.run()
